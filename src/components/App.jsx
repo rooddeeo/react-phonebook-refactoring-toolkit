f499623a9cee460a5d7b8bee485e058041430ade
rooddeeo <rooddeeo@gmail.com>
2023-12-14 20:53:23 +0200

    React Home Work 6

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,25 @@
 import ContactList from './ContactList/ContactList.jsx';
 import { Provider } from 'react-redux';
-import { store } from 'store/store.js';
+import { store, persistor } from 'store/store.js';
 import ContactForm from './ContactForm/ContactForm.jsx';
 import Filter from './Filter/Filter.jsx';
 import css from './App.module.css';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <div className={css.container}>
-        <div className={css.content}>
-          <h1 className={css.title}>Phonebook</h1>
-          <ContactForm />
-          <h2 className={css.subTitle}>Search for contact</h2>
-          <Filter />
-          <ContactList />
+      <PersistGate loading={null} persistor={persistor}>
+        <div className={css.container}>
+          <div className={css.content}>
+            <h1 className={css.title}>Phonebook</h1>
+            <ContactForm />
+            <h2 className={css.subTitle}>Search for contact</h2>
+            <Filter />
+            <ContactList />
+          </div>
         </div>
-      </div>
+      </PersistGate>
     </Provider>
   );
 };
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,5 +1,16 @@
 import { contactsReducer } from './contacts/contactsSlice';
+import storage from 'redux-persist/lib/storage';
+import persistReducer from 'redux-persist/es/persistReducer';
+
+const persistConfig = {
+  key: 'contacts',
+  storage,
+  whitelist: ['contacts'],
+  blacklist: [''],
+};
+
+const persistedReducer = persistReducer(persistConfig, contactsReducer);
 
 export const reducer = {
-  contacts: contactsReducer,
+  contacts: persistedReducer,
 };
